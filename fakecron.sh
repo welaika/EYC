33116c4ed3ce58d398f5aebb8d298a5319cd1f6f
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-01-08 15:53:55 +0100

    the fucking script

---
+++ fakecron.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+selfpath=$(dirname $0)
+source ${selfpath}/lib.sh
+
+while getopts "f:" opt; do
+    case $opt in
+        f) frequency=$OPTARG ;;
+        *) usage ;;
+    esac
+done
+
+checkfreq
+
+for script in `ls ${selfpath}/${frequency}/*.sh`; do
+  launcher $script
+done
