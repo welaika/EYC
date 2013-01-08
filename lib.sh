50126266ec854941c677fd744bf888ea24eaa567
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-01-08 15:53:44 +0100

    functions library

---
+++ lib.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+function usage(){
+  echo "Must invoke script with -f option; possible args are:"
+  echo "- monthly"
+  echo "- weekly"
+  echo "- daily"
+  echo "- fifteen"
+
+  exit 1
+}
+
+function checkfreq(){
+  [[ $frequency ]] || (
+    echo "frequency not set; error somewhere";
+    exit 1
+    )
+  case $frequency in
+    monthly) ;;
+    weekly) ;;
+    daily) ;;
+    fifteen) ;;
+    *) usage ;;
+  esac
+}
+
+function launcher(){
+  pidname $1
+  createpid && (
+      trap "rm -f ${selfpath}/${frequency}/${pid}" 0 1 2 3 15
+      /bin/bash $1
+    ) 
+}
+
+function pidname(){
+  pid="$(basename $1 .sh).pid"
+}
+
+function checkpid(){
+  [[ -e ${selfpath}/${frequency}/${pid} ]] && return 1 || echo "creating pid";
+}
+
+function createpid(){
+  if [[ `checkpid` ]]; then
+    touch ${selfpath}/${frequency}/${pid}
+    return 0
+  else
+    echo "Script already running: pid ${frequency}/${pid} alredy present" >> ${selfpath}/fakecron.log 2>&1
+    return 1
+  fi
+}
